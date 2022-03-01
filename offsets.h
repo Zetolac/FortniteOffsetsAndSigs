52621a4b1f7133c26d8f3397a094382c07e0747a
Zetolac <58708564+Zetolac@users.noreply.github.com>
2022-03-01 18:32:18 +0100

    19.40

--- offsets.h
+++ offsets.h
@@ -3,12 +3,7 @@
 FTransform GetBoneIndex(DWORD_PTR mesh, int index)
 {
     DWORD_PTR bonearray;
-    bonearray = read<DWORD_PTR>(mesh + 0x4A8);
-
-    if (bonearray == NULL)
-    {
-        bonearray = read<DWORD_PTR>(mesh + 0x4A8 + 0x8);
-    }
+    bonearray = read<DWORD_PTR>(mesh + 0x4C0);
     return  read<FTransform>(bonearray + (index * 0x30));
 }
 
@@ -24,15 +19,16 @@ Vector3 GetBoneWithRotation(DWORD_PTR mesh, int id)
 	return Vector3(Matrix._41, Matrix._42, Matrix._43);
 }
 
-#define UWORLD 0xb613240
+#define UWORLD 0xb978aa8
 #define GameInstance_offset 0x190
-#define PlayerState_offset 0x238
+#define PlayerState_offset 0x240
 #define LocalPawn_offset 0x2B0
 #define TeamIndex_offset 0xF50
 #define Levels_offset 0x148
 #define LocalPlayers_offset 0x38 
-#define RootComponent 0x138
-#define Mesh 0x288
+#define RootComponent_offset 0x138
+#define Mesh_offset 0x288
+#define BoneArray_offset 0x4C0
 
 //Bones ID
 Vector3 vHeadBoneOut = ProjectWorldToScreen(GetBoneWithRotation(entity.mesh, 68));
