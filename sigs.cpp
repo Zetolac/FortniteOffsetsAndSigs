0a5d09f624af396d2c7d66ac38cd0936dd910e03
Zetolac <58708564+Zetolac@users.noreply.github.com>
2022-03-20 13:45:57 +0100

    20.00
    20.00

--- sigs.cpp
+++ sigs.cpp
@@ -1,15 +1,1 @@
-Uworld: 48 89 05 ? ? ? ? 48 8B 4B 78 --- \x48\x89\x05\x00\x00\x00\x00\x48\x8B\x4B\x78\ xxx????xxxx
-GoObject: 48 8B 05 ? ? ? ? 48 8B 0C C8 48 8B 04 D1 --- \x48\x8B\x05\x00\x00\x00\x00\x48\x8B\x0C\xC8\x48\x8B\x04\xD1 xxx????xxxxxxxx
-ProjectWorldToScreen: E8 ? ? ? ? 48 8B 5C 24 ? 41 88 07 48 83 C4 30 ---- \xE8\x00\x00\x00\x00\x48\x8B\x5C\x24\x00\x41\x88\x07\x48\x83\xC4\x30 x????xxxx?xxxxxxx
-GetBoneMatrix: E8 ? ? ? ? 48 8B 47 30 F3 0F 10 45 --- \xE8\x00\x00\x00\x00\x48\x8B\x47\x30\xF3\x0F\x10\x45 x????xxxxxxxx
-FortniteFree: 48 85 C9 0F 84 ? ? ? ? 53 48 83 EC 20 48 89 7C 24 30 48 8B D9 48 8B 3D ? ? ? ? 48 85 FF 0F 84 ? ? ? ? 48 8B 07 4C 8B 40 30 48 8D 05 ? ? ? ? 4C 3B C0
-GetNameByID = 48 89 5C 24 ? 48 89 6C 24 ? 56 57 41 56 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 48 8B F2 4C 8B F1 E8 ? ? ? ? 45 8B 06 33 ED 41 0F B7 16 41 C1 E8 10 89 54 24 24 44 89 44 24 ? 48 8B 4C 24 ? 48 C1 E9 20 8D 3C 09 4A 03 7C C0 ? 0F B7 17 C1 EA 06 41 39 6E 04
-CalculateSpreadHook : 
-SpreadCaller : 
-CalculateShot :
-GetPlayerViewPoint : 
-
-17.00 : OK
-18.00 : Nothing change
-18.40 : Nothing change
-19.01 : New Sigs
+Soon
