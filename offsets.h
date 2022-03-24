7f4ba89f97551b6eff8ea4380ee3952d43c50440
Zetolac <58708564+Zetolac@users.noreply.github.com>
2022-03-24 22:46:07 +0100

    20.00
    20.00

--- offsets.h
+++ offsets.h
@@ -1,8 +1,23 @@
-0xC4461F8 -> Uworld
-0xC483140 -> FNamePool 
+GWorld: 0xC446238
+FNamePool: 0xC483180
+UObjectArray: 0xC26E608
+GetNameByIndex: 0xE04400
+GetBoneMatrix: 0x15612FC
+ProjectWorldToScreen: 0x85486C4
+
+OFFSETS:
+BoneArray: 0x5B0
+CompToWorld: 0x250
+GameInstance 0x190
+AcknowledgedPawn 0x338
+RelativeLocation 0x138
+RelativeRotation 0x150
+TeamIndex 0x1018
+PlayerState 0x2B8
+Mesh 0x318
 
 VTABLE:
-0x1F -> ProcessEvent index
+0x4B -> ProcessEvent index
 0x350 -> PlayerCameraManager
 0x6F0 -> GetLastFrameCameraCachePOV
 0x6E8 -> GetCameraCachePOV
