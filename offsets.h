043b32f777df5bb98d99c5123b856208c719a50c
Zetolac <58708564+Zetolac@users.noreply.github.com>
2022-06-05 13:13:24 +0200

    Updated to 21.00

--- offsets.h
+++ offsets.h
@@ -1,24 +1,26 @@
-GWorld: 0xc526e18
-FNamePool: 0xc563880
-UObjectArray: 0xc376110
-GetBoneMatrix: 0xf77c08
-ProjectWorldToScreen: 0x829d314
+GNames = 0xCCB6E80
+GOBjects = 0xCABA728
+GWorld = 0xCC65A08
+Free = 0x0DB3210
+GetBoneMatrix = 0x0FCF3A0
 
 OFFSETS:
-BoneArray: 0x590
+BoneArray: 0x590 | NOT SURE
+
 CompToWorld: 0x240
-GameInstance 0x190
+GameInstance 0x1A8
 RootComp: 0x188
 AcknowledgedPawn 0x310
 RelativeLocation 0x128
 RelativeRotation 0x140
-TeamIndex 0x1010
+TeamIndex 0x1068
+Current Weapon 0x2F0
 PlayerState 0x290
 Mesh 0x2F0
-bIsDBNO 0x6F2
-bIsDying 0x680
-ReviveFromDBNOTime 0x3EE8
-bUseGravity(ud vehicle fly) 0x668
+bIsDBNO 0x74A
+bIsDying 0x6D8
+ReviveFromDBNOTime 0x3FC8
+bUseGravity(ud vehicle fly) 0x672
 
 VTABLE: // MAYBE WRONG
 0x4B -> ProcessEvent index
