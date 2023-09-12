4a59dfa9051fdc32fac356eb2dad3c556f5dc75c
Zetolac <58708564+Zetolac@users.noreply.github.com>
2023-09-12 19:58:20 +0200

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -25,17 +25,3 @@ namespace MeshAndUSceneComponentOffsets
     DWORD IsCached = 0x648;
 };
 
-// New Viewpoint Decryption (UNTESTED) (UNSURE)
-CamewaDescwipsion GetViewPoint(ProtectedClass* p)
-{
-    __int64 result; // rax
-    __int64 v7; // rcx
- 
-    result = p->Read<__int64>(base_address + 0xE2CED18);
-    v7 = base_address + 0xE2CECD8;
-    if (result)
-        v7 = result;
- 
-    __int64 CameraBuffer = p->Read<__int64>(p->Read<__int64>(v7) + 8) ^ 0x4CF1F15DFE2D977Fi64;
-    return p->Read<CamewaDescwipsion>(CameraBuffer);
-}
