12cae4173ce09c950ad89f76a837724fa81cfd31
Zetolac <58708564+Zetolac@users.noreply.github.com>
2023-09-12 17:45:14 +0200

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,37 +1,4 @@
-
-struct CamewaDescwipsion
-{
-	Vector3 Location;
-	Vector3 Rotation;
-	float FieldOfView;
-	char Useless[0x18];
-};
-CamewaDescwipsion GetViewPoint()
-{
-	char v1; // r8
-	CamewaDescwipsion ViewPoint = Read<CamewaDescwipsion>(base_address + 0xed57310);
-	BYTE* v2 = (BYTE*)&ViewPoint;
-	int i; // edx
-	__int64 result; // rax
- 
-	v1 = 0x40;
-	for (i = 0; i < 0x40; ++i)
-	{
-		*v2 ^= v1;
-		result = (unsigned int)(i + 0x17);
-		v1 += i + 0x17;
-		v2++;
-	}
- 
-	
-
-#define GWorld 0xee2f8e8
-#define FNamePool 0xeeb2c80
-#define GetNameById 0xd1d6ac
-#define DecryptAnsi 0x569fc70
-#define ProjectWorldLocationToScreen 0x7ac9d04
-#define EncryptedViewPoint 0xed57310
-
+#define GWorld 0xe762638
 namespace WorldOffsets
 {
     DWORD PersitentLevel = 0x30;
@@ -44,18 +11,12 @@ namespace ActorOffsets
 };
 namespace PlayerControllerOffsets
 {
-    DWORD AcknowledgedPawn = 0x338;
-    DWORD PlayerCameraManager = 0x348;
     DWORD RotationInput = 0x520;
 };
 namespace FortPawnOffsets
 {
     DWORD CurrentWeapon = 0x948;
-    DWORD ReviveFromDBNOTime = 0x46b8;
-};
-namespace PlayerStateOffsets
-{
-    DWORD TeamIndex = 0x10d0;
+    DWORD ReviveFromDBNOTime = 0x4af8;
 };
 namespace MeshAndUSceneComponentOffsets
 {
@@ -63,3 +24,18 @@ namespace MeshAndUSceneComponentOffsets
     DWORD BoneArray = 0x600;
     DWORD IsCached = 0x648;
 };
+
+// New Viewpoint Decryption (UNTESTED) (UNSURE)
+CamewaDescwipsion GetViewPoint(ProtectedClass* p)
+{
+    __int64 result; // rax
+    __int64 v7; // rcx
+ 
+    result = p->Read<__int64>(base_address + 0xE2CED18);
+    v7 = base_address + 0xE2CECD8;
+    if (result)
+        v7 = result;
+ 
+    __int64 CameraBuffer = p->Read<__int64>(p->Read<__int64>(v7) + 8) ^ 0x4CF1F15DFE2D977Fi64;
+    return p->Read<CamewaDescwipsion>(CameraBuffer);
+}
