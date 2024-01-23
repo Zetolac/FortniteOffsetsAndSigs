ef33a91d203b0640b9bb25c4867c86f928ac686b
Zetolac <58708564+Zetolac@users.noreply.github.com>
2024-01-23 11:53:11 +0100

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,27 +1,20 @@
-#define GWorld 0x11781328
-namespace WorldOffsets
-{
-    DWORD PersitentLevel = 0x30;
-    DWORD GameInstance = 0x1d0;
-};
-namespace ActorOffsets
-{
-    DWORD PlayerState = 0x2b0;
-    DWORD Mesh = 0x318;
-};
-namespace PlayerControllerOffsets
-{
-    DWORD RotationInput = 0x520;
-};
-namespace FortPawnOffsets
-{
-    DWORD CurrentWeapon = 0xa20;
-    DWORD ReviveFromDBNOTime = 0x4ae8;
-};
-namespace MeshAndUSceneComponentOffsets
-{
-    DWORD ComponentToWorld = 0x230;
-    DWORD BoneArray = 0x608;
-    DWORD IsCached = 0x650;
-};
 
+#define FNamePool 0x1150A280
+#define GWorld 0x1133C4C8
+
+char* v2 = buff; // rdi
+int v4 = 17; // ecx
+int v5 = nameLength; // eax
+__int64 v6; // rdx
+
+if (v5)
+{
+	v6 = v5;
+	do
+	{
+		v5 = v4 + ~*v2;
+		v4 += 0x1F0B;
+		*v2++ = v5;
+		--v6;
+	} while (v6);
+}
