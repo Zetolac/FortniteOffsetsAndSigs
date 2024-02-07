72bb8cc26c425f983ee916ee27598ced2694f697
Zetolac <58708564+Zetolac@users.noreply.github.com>
2024-02-07 18:16:33 +0100

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,20 +1,6 @@
+ComponentToWorld : 0x230
+__int64 BoneMap = read<uintptr_t>(mesh + 0x598 + ( read<int>(mesh + 0x5e0) * 0x10));
+FTransform Bone = read<FTransform>(BoneMap + 0x60 * id)
 
-#define FNamePool 0x1150A280
-#define GWorld 0x1133C4C8
-
-char* v2 = buff; // rdi
-int v4 = 17; // ecx
-int v5 = nameLength; // eax
-__int64 v6; // rdx
-
-if (v5)
-{
-	v6 = v5;
-	do
-	{
-		v5 = v4 + ~*v2;
-		v4 += 0x1F0B;
-		*v2++ = v5;
-		--v6;
-	} while (v6);
-}
+#define FNamePool 0x11944BC0
+#define GWorld 0x11770D88
