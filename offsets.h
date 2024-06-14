7febf9b25e4447f5e4ed6296e0bf84e08f33fabb
Zetolac <58708564+Zetolac@users.noreply.github.com>
2024-06-14 11:13:31 +0200

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,7 +1,13 @@
+0x12B9C140 -< GNames
+0x129A7990 -< GObjects
+0x1287CF68 -< GEngine
+0x129CFDD8 -< GWorld
 
-ComponentToWorld : 0x230
-__int64 BoneMap = read<uintptr_t>(mesh + 0x5b0 + ( read<int>(mesh + 0x5f8) * 0x10));
-FTransform Bone = read<FTransform>(BoneMap + 0x60 * id)
 
-#define FNamePool 0x126A01C0
-#define GWorld 0x124BF388
+Exemple:
+__int64 va_text = 0;
+	for (int i = 0; i < 100; i++)
+		if (read<__int32>(f_ProcessBase + (i * 0x1000)) == 0x24448B48) { va_text = f_ProcessBase + (i * 0x1000); break; }
+__int64 GWorld = 0x129CFDD8 + va_text;
+__int64 UWorld = read<__int64>(GWorld);
+__int64 GameInstance = read<__int64>(UWorld + 0x1D8);
