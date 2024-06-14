87727b0054f6e6529d3384dac2d18def795f9166
Zetolac <58708564+Zetolac@users.noreply.github.com>
2024-06-14 15:16:06 +0200

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -5,9 +5,9 @@
 
 
 Exemple:
-__int64 va_text = 0;
-	for (int i = 0; i < 100; i++)
-		if (read<__int32>(f_ProcessBase + (i * 0x1000)) == 0x24448B48) { va_text = f_ProcessBase + (i * 0x1000); break; }
+  __int64 va_text = 0;
+    for (int i = 0; i < 25; i++)
+        if (read<__int32>(base + (i * 0x1000) + 0x250) == 0x260E020B) { va_text = base + ((i + 1) * 0x1000);}
 __int64 GWorld = 0x129CFDD8 + va_text;
 __int64 UWorld = read<__int64>(GWorld);
 __int64 GameInstance = read<__int64>(UWorld + 0x1D8);
