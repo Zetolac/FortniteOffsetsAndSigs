167d9acb3295559c22609e1a3dd91b12a61ade31
Zetolac <58708564+Zetolac@users.noreply.github.com>
2024-06-18 16:55:38 +0200

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,7 +1,6 @@
-0x12B9C140 -< GNames
-0x129A7990 -< GObjects
-0x1287CF68 -< GEngine
-0x129CFDD8 -< GWorld
+0x129DEDD8 <- GWorld
+0x12A61590 <- GObjects
+0x12BAE1C0 <- GNames
 
 
 Exemple:
@@ -11,3 +10,29 @@ Exemple:
 __int64 GWorld = 0x129CFDD8 + va_text;
 __int64 UWorld = read<__int64>(GWorld);
 __int64 GameInstance = read<__int64>(UWorld + 0x1D8);
+
+
+DecryptAnsi:
+ _WORD *v3; // rdi@1
+  signed int v5; // edx@1
+  __int64 result; // rax@1
+  __int64 v7; // r8@2
+  unsigned int v8; // edx@3
+ 
+v3 = buffer;
+  v5 = 26;
+  result = size;
+  if ( (_DWORD)result )
+  {
+    v7 = (unsigned int)result;
+    do
+    {
+      v8 = v5 + 45297;
+      *v3 = v8 + ~*v3;
+      result = v8 << 8;
+      ++v3;
+      v5 = result | (v8 >> 8);
+      --v7;
+    }
+    while ( v7 );
+  }
