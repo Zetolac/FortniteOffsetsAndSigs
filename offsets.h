f525d4269860ccbcbc4aa285e88df3e3d36388d8
Zetolac <58708564+Zetolac@users.noreply.github.com>
2024-06-22 12:51:48 +0200

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,38 +1,42 @@
-0x129DEDD8 <- GWorld
-0x12A61590 <- GObjects
-0x12BAE1C0 <- GNames
+0x11558538 <- GWorld
+0x117284C0 <- GNames
+usage: .text address + offset
 
 
 Exemple:
   __int64 va_text = 0;
     for (int i = 0; i < 25; i++)
         if (read<__int32>(base + (i * 0x1000) + 0x250) == 0x260E020B) { va_text = base + ((i + 1) * 0x1000);}
-__int64 GWorld = 0x129CFDD8 + va_text;
+__int64 GWorld = 0x11558538 + va_text;
 __int64 UWorld = read<__int64>(GWorld);
 __int64 GameInstance = read<__int64>(UWorld + 0x1D8);
 
 
 DecryptAnsi:
- _WORD *v3; // rdi@1
-  signed int v5; // edx@1
-  __int64 result; // rax@1
-  __int64 v7; // r8@2
-  unsigned int v8; // edx@3
+
+_WORD *v2; // rdi
+  int v4; // ebx
+  unsigned int v5; // eax
+  unsigned int v6; // r8d
+  __int64 result; // rax
+  unsigned int v8; // kr00_4
+  int v9; // ecx
  
-v3 = buffer;
-  v5 = 26;
-  result = size;
-  if ( (_DWORD)result )
+  v2 = buffer;
+  v4 = size;
+  v5 = read<unsigned int>(va_text + 0x1162E03C);
+  v6 = v5 ^ 0x9C677CC5;
+  v8 = v5;
+  result = 1041204193 * v5;
+  v9 = 0;
+  if ( v4 )
   {
-    v7 = (unsigned int)result;
     do
     {
-      v8 = v5 + 45297;
-      *v3 = v8 + ~*v3;
-      result = v8 << 8;
-      ++v3;
-      v5 = result | (v8 >> 8);
-      --v7;
+      result = v9 + v8 % 0x21;
+      ++v9;
+      v6 += result;
+      *v2++ ^= v6;
     }
-    while ( v7 );
+    while ( v9 < v4 );
   }
